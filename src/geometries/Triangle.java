f8003d7911bf2f073ab8e55b08e05582f335bcbb
netanellyons <48799300+netanellyons@users.noreply.github.com>
2019-03-29 13:03:05 +0300

    Update Triangle.java

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -22,9 +22,7 @@ public class Triangle implements Geometry {
 
     //copy constructor
     public Triangle(Triangle t) {
-        _p1 = t._p1;
-        _p2 = t._p2;
-        _p3 = t._p3;
+        this(new Point3D(t.get_p1()),new Point3D(t.get_p2()),new Point3D(t.get_p3()));
     }
 
     //empty constructor
@@ -61,13 +59,10 @@ public class Triangle implements Geometry {
 
     @Override
     public Vector getNormal(Point3D p) {
-        Point3D p1c = new Point3D(_p1);
         Point3D p2c = new Point3D(_p2);
         Point3D p3c = new Point3D(_p3);
-        p2c.subtract(p1c);
-        Vector vector1 = new Vector(p2c);
-        p3c.subtract(p1c);
-        Vector vector2 = new Vector(p3c);
+        Vector vector1 = new Vector(p2c.subtract(get_p1()));
+        Vector vector2 = new Vector(p3c.subtract(get_p1()));
         Vector crossProduct = vector1.crossProduct(vector2);
         crossProduct.normalize();
         return crossProduct;
@@ -82,26 +77,21 @@ public class Triangle implements Geometry {
         Plane planeP = new Plane(_p1, getNormal(get_p1()));
         Point3D p = planeP.findIntersections(r).get(0);
         Point3D cpyP = new Point3D(p);
-        cpyP1.subtract(r.get_p00());
-        cpyP2.subtract(r.get_p00());
-        cpyP3.subtract(r.get_p00());
-        Vector v1 = new Vector(cpyP1);
-        Vector v2 = new Vector(cpyP2);
-        Vector v3 = new Vector(cpyP3);
-
+       
+        Vector v1 = new Vector(cpyP1.subtract(r.get_p00()));
+        Vector v2 = new Vector(cpyP2.subtract(r.get_p00()));
+        Vector v3 = new Vector(cpyP3.subtract(r.get_p00()));
 
         Vector cross1 = new Vector(v2.crossProduct(v1));
         Vector cross2 = new Vector(v3.crossProduct(v1));
         Vector cross3 = new Vector(v2.crossProduct(v3));
-        cross1.scaling(1 / v2.crossProduct(v1).length());
-        cross2.scaling(1 / v3.crossProduct(v1).length());
-        cross3.scaling(1 / v2.crossProduct(v3).length());
+        cross1.scaling(1 / cross1.length());
+        cross2.scaling(1 / cross2.length());
+        cross3.scaling(1 / cross3.length());
         Vector N1 = new Vector(cross1);
         Vector N2 = new Vector(cross2);
         Vector N3 = new Vector(cross3);
-
-        cpyP.subtract(r.get_p00());
-        Vector v = new Vector(cpyP);
+        Vector v = new Vector(cpyP.subtract(r.get_p00()));
         if ((sign(v.dotProduct(N1)) == sign(v.dotProduct(N2)))&& sign(v.dotProduct(N1)) == sign(v.dotProduct(N3)))
             l.add(p);
         return l;
