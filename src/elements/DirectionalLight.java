88fb3cdae48bc7ad6ecf513f6281ac2acc4b3a9a
netanellyons <48799300+netanellyons@users.noreply.github.com>
2019-05-11 22:19:10 +0300

    Update DirectionalLight.java

--- src/elements/DirectionalLight.java
+++ src/elements/DirectionalLight.java
@@ -5,22 +5,32 @@ import primitives.Vector;
 
 public class DirectionalLight extends Light {
 
+
+
     //direction of light
     private Vector direction;
 
-    public Vector getDirection() {
-        return direction;
-    }
-
+    //full constructor
     public DirectionalLight(Vector direction, Color color) {
         super(color);
         this.direction = direction;
     }
+
+    //empty constructor
     public DirectionalLight(){
         super();
     }
 
+    //getters and setters
+    public Vector getDirection() {
+        return direction;
+    }
 
+    public void setDirection(Vector direction) {
+        this.direction = direction;
+    }
+
+    //override getIntensity
     @Override
     public Color getIntensity() {
 
