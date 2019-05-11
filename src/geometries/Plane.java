8fe9efae8c387125174905d5565bc71380dc763a
netanellyons <48799300+netanellyons@users.noreply.github.com>
2019-05-11 22:22:24 +0300

    Update Plane.java

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -8,11 +8,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 //represents a plane in space
-public class Plane implements Geometry{
+public class Plane extends Geometry{
+    //Point representing plane in space
     private Point3D _p;
-    private Vector _normal;// the normal vector from the point to create a 90 degree angle and therefore represent a plane
+    //Normal to the point of the plane
+    private Vector _normal;
 
-    //constructor
+    //full constructor
     public Plane(Point3D _p, Vector _normal) {
         this._p = _p;
         this._normal = _normal;
@@ -20,7 +22,7 @@ public class Plane implements Geometry{
 
     //copy constructor
     public Plane(Plane p) {
-       this(new Point3D(p.get_p()),new Vector(p.get_normal()));
+        this(new Point3D(p.get_p()),new Vector(p.get_normal()));
     }
 
     //empty constructor
@@ -46,28 +48,40 @@ public class Plane implements Geometry{
         this._normal = _normal;
     }
 
+    //override getNormal function
     @Override
     public Vector getNormal(Point3D p) {
         return _normal;
     }
 
+    //override findIntersections function
     @Override
     public List<Point3D> findIntersections(Ray r) {
-        List l = new ArrayList();                                       //initialize list
-        Point3D temp =  new Point3D(r.get_p00());                       //copy points and vectors
+        //create new list
+        List l = new ArrayList();
+        //copy variables so they don't change
+        Point3D temp =  new Point3D(r.get_p00());
         Point3D temp2 =  new Point3D(r.get_p00());
         Vector cpyNormal= new Vector(_normal);
         Vector cpyDirection = new Vector(r.get_direction());
-        double NDotV = (r.get_direction().dotProduct(get_normal()));    // (N dotProduct V)
-        Vector tempV = new Vector(temp.subtract(get_p()));              //(P0-Qo)
+        //calculate dot product of normal to plane and ray
+        double NDotV = (r.get_direction().dotProduct(get_normal()));
+        //create vector of P0-Q0
+        Vector tempV = new Vector(temp.subtract(get_p()));
+        //if plane is parallel to ray...
         if (NDotV == 0)
             return l;
-        tempV.scaling(1/NDotV);                                         //(P0-Qo)/(N dotProduct V)
-        cpyNormal.scaling(-1);                                          //-N
-        double t = cpyNormal.dotProduct(tempV);                         //obtain scalar t
-        cpyDirection.scaling(t);                                        //obtain vector of intersection
+        //scale vector and normal
+        tempV.scaling(1/NDotV);
+        cpyNormal.scaling(-1);
+        //obtain dot product of normal and new vector
+        double t = cpyNormal.dotProduct(tempV);
+        //scale direction vector by result
+        cpyDirection.scaling(t);
+        //obtain point of intersection
         Point3D P = new Point3D(temp2.add(cpyDirection));
-        l.add(P);                                                       //add to list
+        //add point to list and return list
+        l.add(P);
         return l;
     }
 }
