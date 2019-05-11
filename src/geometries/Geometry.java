a90cf54b116748c4a25134945630ba4b9240bd6d
netanellyons <48799300+netanellyons@users.noreply.github.com>
2019-05-11 22:21:12 +0300

    Update Geometry.java

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -7,10 +7,16 @@ import primitives.Vector;
 
 import java.util.List;
 
+
+
+//interface geometry
 public abstract class Geometry {
 
-private Material _material;
+    //field material
+    private Material _material;
 
-   public abstract Vector getNormal(Point3D p);
-   public abstract List<Point3D> findIntersections(Ray r);
+    //return normal
+    public abstract Vector getNormal(Point3D p);
+    //return list of intersections
+    public abstract List<Point3D> findIntersections(Ray r);
 }
