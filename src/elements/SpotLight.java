873ffe60193960e2685ab6e3fa503065162c92b1
netanellyons <48799300+netanellyons@users.noreply.github.com>
2019-05-25 21:47:31 +0300

    Update SpotLight.java

--- src/elements/SpotLight.java
+++ src/elements/SpotLight.java
@@ -11,11 +11,13 @@ public class SpotLight extends PointLight{
     //full constructor
     public SpotLight(Color color, Point3D _position, double _Kc, double _Kl, double _Kq, Vector _direction) {
         super(color, _position, _Kc, _Kl, _Kq);
+        _direction.normalize();
         this._direction = _direction;
     }
 
     //constructor
     public SpotLight(Vector _direction) {
+        _direction.normalize();
         this._direction = _direction;
     }
 
@@ -25,20 +27,35 @@ public class SpotLight extends PointLight{
     }
     //getters and setters
     public Vector get_direction() {
+        _direction.normalize();
         return _direction;
     }
 
     public void set_direction(Vector _direction) {
+        _direction.normalize();
         this._direction = _direction;
     }
 
     //override getIntensity
-    public Color getIntensity() {
-        double d = 1;
-        //TODO fix this
-        double dotProduct = get_direction().dotProduct(new Vector());
+    @Override
+    public Color getIntensity(Point3D p) {
+        //copy color to maintain variable
+        Color copy = new Color(get_color());
+        //find distance between light and geometry
+        double d = p.distance(get_position());
+        //obtain dot product of D and L
+        double dotProduct = Math.max(0,get_direction().dotProduct(getL(p)));
+        //obtain denominator of function IL=[I0(D.L)/Kc+Kl(d)+Kq(d^2)]
+        double denominator = (get_Kc()+(get_Kl()*d) + (get_Kq()*(d*d)));
+        //scale color by dot product
+        copy.scale(dotProduct);
+        //scale color by denominator to obtain intensity
+        return copy.scale(1/denominator);
+    }
 
-        double temp = get_Kc()+get_Kl()*d + get_Kq()*(d*d);
-        return get_color().scale(1/temp);
+    //override get L function
+    @Override
+    public Vector getL(Point3D p){
+       return super.getL(p);
     }
 }
