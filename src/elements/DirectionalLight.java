c12f4d235f426232d3e48536eb9a9c15ebf3c8e1
netanellyons <48799300+netanellyons@users.noreply.github.com>
2019-05-25 21:46:45 +0300

    Update DirectionalLight.java

--- src/elements/DirectionalLight.java
+++ src/elements/DirectionalLight.java
@@ -1,18 +1,17 @@
 package elements;
 
 import primitives.Color;
+import primitives.Point3D;
 import primitives.Vector;
 
-public class DirectionalLight extends Light {
-
-
-
+public class DirectionalLight extends Light implements LightSource {
     //direction of light
     private Vector direction;
 
     //full constructor
     public DirectionalLight(Vector direction, Color color) {
         super(color);
+        direction.normalize();
         this.direction = direction;
     }
 
@@ -23,17 +22,32 @@ public class DirectionalLight extends Light {
 
     //getters and setters
     public Vector getDirection() {
+        direction.normalize();
         return direction;
     }
 
     public void setDirection(Vector direction) {
+        direction.normalize();
         this.direction = direction;
     }
 
     //override getIntensity
     @Override
     public Color getIntensity() {
-
         return get_color();
+    }
+
+    //override getIntensity with point
+    @Override
+    public Color getIntensity(Point3D p) {
+        return null;
+    }
+
+    //override get L vector
+    @Override
+    public Vector getL(Point3D p) {
+        Vector copyDirection = new Vector(getDirection());
+        copyDirection.normalize();
+        return copyDirection;
     }
 }
